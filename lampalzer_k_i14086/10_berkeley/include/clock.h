8cac285ddc39ca54f45b9d61afbd964168ac3304
Konstantin Lampalzer <konstantin@localhost.systems>
2019-01-07 09:37:31 +0100

    Ex 10.15

--- lampalzer_k_i14086/10_berkeley/include/clock.h
+++ lampalzer_k_i14086/10_berkeley/include/clock.h
@@ -15,14 +15,13 @@ class Clock
     chrono::time_point<chrono::system_clock> curr_time;
     string name;
     static mutex mtx;
-    
+    bool monotonTime;
+    int clockSlowdown = 0;
+
   public:
     static int clockSpeed;
-    
     Clock(){};
-    Clock(string _name) : name{_name}
-    {
-    };
+    Clock(string _name) : name{_name} {};
 
     Clock(string _name, int hours, int minutes, int seconds)
     {
@@ -39,14 +38,28 @@ class Clock
                 lock_guard<mutex> lg(mtx);
                 cout << name << ": " << curr_time << endl;
             }
-            this_thread::sleep_for(chrono::milliseconds(clockSpeed));
+            this_thread::sleep_for(chrono::milliseconds(clockSpeed + clockSlowdown));
             curr_time += 1s;
         }
     }
 
+    void setTimeMonoton(bool value)
+    {
+        monotonTime = value;
+    }
+
     void set_time(int hours, int minutes, int seconds)
     {
-        curr_time = TIME_UTILS_H::set_time(curr_time, hours, minutes, seconds);
+        chrono::time_point<chrono::system_clock> newTime = TIME_UTILS_H::set_time(curr_time, hours, minutes, seconds);
+        if (newTime < curr_time)
+        {
+            clockSlowdown = 250;
+        }
+        else
+        {
+            clockSlowdown = 0;
+            curr_time = TIME_UTILS_H::set_time(curr_time, hours, minutes, seconds);
+        }
     }
 
     tuple<int, int, int> get_time()
